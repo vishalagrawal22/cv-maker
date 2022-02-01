ca3e6eb89297e2f4eb66b03cdd4cf03d5f56c2ff
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-02-01 16:57:53 +0530

    Add EducationSection

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,3 +1,6 @@
+import { Component } from 'react';
+import { Section } from './Section';
+import { renderIfTrue } from '../utils/helper-functions';
 import uniqid from 'uniqid';
 
 function EducationFactory(
@@ -11,4 +14,48 @@ function EducationFactory(
   return { institute, degree, marks, start, end, id };
 }
 
-export { EducationFactory };
+class EducationItem extends Component {
+  render() {
+    const { education } = this.props;
+    return (
+      <li>
+        <div>{education.institute}</div>
+        <div>
+          <div>
+            {education.degree}
+            {renderIfTrue(
+              education.marks !== '',
+              <span> - {education.marks}</span>
+            )}
+          </div>
+        </div>
+        <div>
+          {renderIfTrue(
+            education.start !== '',
+            <span>{education.start} - </span>
+          )}
+          {renderIfTrue(education.end !== '', <span>{education.end}</span>)}
+          {renderIfTrue(
+            education.end === '' && education.start !== '',
+            <span>{'Present'}</span>
+          )}
+        </div>
+      </li>
+    );
+  }
+}
+
+class EducationSection extends Component {
+  render() {
+    const { educations } = this.props;
+    return (
+      <Section header="Education">
+        {educations.map((education) => (
+          <EducationItem key={education.id} education={education} />
+        ))}
+      </Section>
+    );
+  }
+}
+
+export { EducationFactory, EducationItem, EducationSection };
