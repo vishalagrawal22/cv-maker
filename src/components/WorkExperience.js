4915aebc2f78948f7e3729bee6eb7ec78536e098
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-02-01 17:44:48 +0530

    Add WorkExperience Component

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,3 +1,6 @@
+import { Component } from 'react';
+import { Section } from './Section';
+import { renderIfTrue, conditionalRender } from '../utils/helper-functions';
 import uniqid from 'uniqid';
 
 function WorkExperienceFactory(
@@ -11,4 +14,46 @@ function WorkExperienceFactory(
   return { company, position, description, start, end, id };
 }
 
-export { WorkExperienceFactory };
+class WorkExperienceItem extends Component {
+  render() {
+    const { workExperience } = this.props;
+    return (
+      <li>
+        <div>{workExperience.company}</div>
+        <div>{workExperience.positon}</div>
+        {renderIfTrue(
+          workExperience.description !== '',
+          <div>{workExperience.description}</div>
+        )}
+        <div>
+          <span>
+            {workExperience.start} -
+            {conditionalRender(
+              workExperience.end !== '',
+              <span> {workExperience.end}</span>,
+              <span> {'Present'}</span>
+            )}
+          </span>
+        </div>
+      </li>
+    );
+  }
+}
+
+class WorkExperienceSection extends Component {
+  render() {
+    const { workExperiences } = this.props;
+    return (
+      <Section header="Work Experience">
+        {workExperiences.map((workExperience) => (
+          <WorkExperienceItem
+            key={workExperience.id}
+            workExperience={workExperience}
+          />
+        ))}
+      </Section>
+    );
+  }
+}
+
+export { WorkExperienceFactory, WorkExperienceItem, WorkExperienceSection };
