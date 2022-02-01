4fb7348d79c0fe20fb014e2ec69856f6a09bcf7e
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-02-01 20:22:52 +0530

    Add UserInfoSection

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,5 +1,21 @@
+import { Component } from 'react';
+import { Section } from './Section';
+
 function UserInfoFactory(name, email, phone) {
   return { name, email, phone };
 }
 
-export { UserInfoFactory };
+class UserInfoSection extends Component {
+  render() {
+    const { userInfo } = this.props;
+    return (
+      <Section header="General Info">
+        <div>Name: {userInfo.name}</div>
+        <div>Email: {userInfo.email}</div>
+        <div>Phone: {userInfo.phone}</div>
+      </Section>
+    );
+  }
+}
+
+export { UserInfoFactory, UserInfoSection };
