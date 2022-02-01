416366a7a200d30db745e4339a4ad0fce405dc41
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-02-01 12:09:43 +0530

    Add ProjectItem Component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,58 @@
+import { Component } from 'react';
+import { renderIfTrue } from '../utils/helper-functions';
+
 function ProjectFactory(name, codeUrl, hostingUrl, techStack, description) {
   return { name, codeUrl, hostingUrl, techStack, description };
 }
 
-export { ProjectFactory };
+class ProjectItem extends Component {
+  render() {
+    const { project } = this.props;
+    return (
+      <div>
+        {renderIfTrue(
+          project.name !== '',
+          <div>Project Name: {project.name}</div>
+        )}
+        {renderIfTrue(
+          project.codeUrl !== '' || project.hostingUrl !== '',
+          <div className="action-links">
+            {renderIfTrue(
+              project.codeUrl !== '',
+              <div>
+                <a href={project.codeUrl} rel="noreferrer" target="_blank">
+                  View Code
+                </a>
+              </div>
+            )}
+            {renderIfTrue(
+              project.hostingUrl !== '',
+              <div>
+                <a href={project.hostingUrl} rel="noreferrer" target="_blank">
+                  Live Preview
+                </a>
+              </div>
+            )}
+          </div>
+        )}
+        {renderIfTrue(
+          project.techStack.length !== 0,
+          <div>
+            Tech Stack:
+            <ul>
+              {project.techStack.map((tech, index) => (
+                <li key={index}>{tech}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {renderIfTrue(
+          project.description !== '',
+          <div>Description: {project.description}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export { ProjectFactory, ProjectItem };
--- src/tests/Project.test.js
+++ src/tests/Project.test.js
@@ -1,4 +1,6 @@
-import { ProjectFactory } from '../components/Project';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { ProjectFactory, ProjectItem } from '../components/Project';
 
 describe('Project Factory tests', () => {
   test('getters work', () => {
@@ -30,5 +32,45 @@ describe('Project Factory tests', () => {
     expect(project.description).toBe(
       'A simple website which shows covid statistics of the world, user’s location or any specific state of any country.'
     );
+  });
+});
+
+describe('ProjectItem component test', () => {
+  test('If provided with everything renders all', () => {
+    const project = ProjectFactory(
+      'Covid Statistics',
+      'https://github.com/vishalagrawal22/covid-statistics',
+      'https://vishalagrawal22.github.io/covid-statistics/',
+      ['Javascript(Jquery)', 'Covid statistics and reverse geocoding APIs'],
+      'A simple website which shows covid statistics of the world, user’s location or any specific state of any country.'
+    );
+    render(<ProjectItem project={project} />);
+
+    const projectName = screen.getByText(/project name/i);
+    const projectCodeUrl = screen.getByText(/view code/i);
+    const projectHostingUrl = screen.getByText(/live preview/i);
+    const projectTechStack = screen.getByText(/tech stack/i);
+    const projectDescription = screen.getByText(/description/i);
+    expect(projectName).toBeInTheDocument();
+    expect(projectCodeUrl).toBeInTheDocument();
+    expect(projectHostingUrl).toBeInTheDocument();
+    expect(projectTechStack).toBeInTheDocument();
+    expect(projectDescription).toBeInTheDocument();
+  });
+
+  test('If not provided info should not be in dom', () => {
+    const project = ProjectFactory('', '', '', [], '');
+    render(<ProjectItem project={project} />);
+
+    const projectName = screen.queryByText(/project name/i);
+    const projectCodeUrl = screen.queryByText(/view code/i);
+    const projectHostingUrl = screen.queryByText(/live preview/i);
+    const projectTechStack = screen.queryByText(/tech stack/i);
+    const projectDescription = screen.queryByText(/description/i);
+    expect(projectName).not.toBeInTheDocument();
+    expect(projectCodeUrl).not.toBeInTheDocument();
+    expect(projectHostingUrl).not.toBeInTheDocument();
+    expect(projectTechStack).not.toBeInTheDocument();
+    expect(projectDescription).not.toBeInTheDocument();
   });
 });
